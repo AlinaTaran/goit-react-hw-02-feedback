9277cbe0a0f73e5795209beaab8c540e2e4c1760
Alina <63247458+AlinaTaran@users.noreply.github.com>
2020-12-08 01:31:05 +0200

    Update FeedbackOptions.js

--- src/components/Feedback/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions.js
@@ -1,23 +1,5 @@
 import PropTypes from 'prop-types';
 
-// const FeedbackBtn = ({ feedback, onLeaveFeedback }) => {
-//   return (
-//     <button type="button" data-feedback={feedback} onClick={onLeaveFeedback}>
-//       {feedback}
-//     </button>
-//   );
-// };
-
-// const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-//   return options.map(option => (
-//     <FeedbackBtn feedback={option} onLeaveFeedback={onLeaveFeedback} />
-//   ));
-// };
-
-// FeedbackOptions.propTypes = {
-//   options: PropTypes.arrayOf(PropTypes.string),
-//   onLeaveFeedback: PropTypes.func.isRequired,
-// };
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map(option => (
